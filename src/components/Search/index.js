56343fef717f7ee873e8471586fc12917a269aef
Craig Wermert <craig@wermert.me>
2023-06-05 16:44:57 -0400

    super simple autocomplete-ish element

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,16 +1,74 @@
 import { useState } from "react";
+import axios from "axios";
+import { useSelect } from "downshift";
 
 import "./Search.css";
 
+const getResults = (val) =>
+  val.length && axios.get(`https://api.npms.io/v2/search/suggestions?q=${val}`);
+
 export const Search = () => {
   const [searchVal, setSearchVal] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  const itemToString = (item) => (item ? item.package.name : "");
+
+  const {
+    isOpen,
+    selectedItem,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    highlightedIndex,
+    getItemProps,
+  } = useSelect({
+    items: suggestions,
+    itemToString,
+  });
+
+  const onInputChange = async (val) => {
+    setSearchVal(val);
+    const results = await getResults(val);
+    console.log(results.data);
+    setSuggestions(results.data);
+  };
 
   return (
-    <input
-      className="searchInput"
-      type="search"
-      value={searchVal}
-      onChange={(e) => setSearchVal(e.target.value)}
-    />
+    <>
+      <input
+        className="searchInput"
+        type="search"
+        value={searchVal}
+        onChange={(e) => onInputChange(e.target.value)}
+      />
+
+      {/* {suggestions?.map((pkg) => (
+        <p>{pkg.package.name}</p>
+      ))} */}
+      <div>
+        <label {...getLabelProps()}>Choose an element:</label>
+        <div {...getToggleButtonProps()}>{selectedItem ?? "Elements"}</div>
+        <ul {...getMenuProps()}>
+          {isOpen &&
+            suggestions.length > 0 &&
+            suggestions.map((item, index) => {
+              return (
+                <li
+                  style={
+                    highlightedIndex === index
+                      ? { backgroundColor: "#bde4ff" }
+                      : {}
+                  }
+                  key={`${item.package.name}${index}`}
+                  {...getItemProps({ item, index })}
+                >
+                  {item.package.name}
+                </li>
+              );
+            })}
+        </ul>
+        <div tabIndex="0" />
+      </div>
+    </>
   );
 };
