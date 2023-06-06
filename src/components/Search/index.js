0064ab23216b97a45265554ec8ff0a049a6d4795
Craig Wermert <craig@wermert.me>
2023-06-06 09:20:08 -0400

    fetching results and displaying in dropdown

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { useSelect } from "downshift";
+import { useCombobox } from "downshift";
 
 import "./Search.css";
 
@@ -8,67 +8,83 @@ const getResults = (val) =>
   val.length && axios.get(`https://api.npms.io/v2/search/suggestions?q=${val}`);
 
 export const Search = () => {
-  const [searchVal, setSearchVal] = useState("");
   const [suggestions, setSuggestions] = useState([]);
-
-  const itemToString = (item) => (item ? item.package.name : "");
 
   const {
     isOpen,
-    selectedItem,
     getToggleButtonProps,
     getLabelProps,
     getMenuProps,
-    highlightedIndex,
+    getInputProps,
     getItemProps,
-  } = useSelect({
+  } = useCombobox({
+    async onInputValueChange({ inputValue }) {
+      if (inputValue.length) {
+        const results = await getResults(inputValue);
+        setSuggestions(results.data);
+      }
+    },
     items: suggestions,
-    itemToString,
+    itemToString(item) {
+      return item ? item.package.name : "";
+    },
   });
 
-  const onInputChange = async (val) => {
-    setSearchVal(val);
-    const results = await getResults(val);
-    console.log(results.data);
-    setSuggestions(results.data);
-  };
-
   return (
-    <>
-      <input
-        className="searchInput"
-        type="search"
-        value={searchVal}
-        onChange={(e) => onInputChange(e.target.value)}
-      />
-
-      {/* {suggestions?.map((pkg) => (
-        <p>{pkg.package.name}</p>
-      ))} */}
-      <div>
-        <label {...getLabelProps()}>Choose an element:</label>
-        <div {...getToggleButtonProps()}>{selectedItem ?? "Elements"}</div>
-        <ul {...getMenuProps()}>
-          {isOpen &&
-            suggestions.length > 0 &&
-            suggestions.map((item, index) => {
-              return (
-                <li
-                  style={
-                    highlightedIndex === index
-                      ? { backgroundColor: "#bde4ff" }
-                      : {}
-                  }
-                  key={`${item.package.name}${index}`}
-                  {...getItemProps({ item, index })}
-                >
-                  {item.package.name}
-                </li>
-              );
-            })}
-        </ul>
-        <div tabIndex="0" />
+    <div>
+      <div className="w-72 flex flex-col gap-1">
+        <label className="w-fit" {...getLabelProps()}>
+          NPM Package Search
+        </label>
+        <div className="flex shadow-sm bg-white gap-0.5">
+          <input
+            placeholder="Search"
+            className="w-full p-1.5 searchInput"
+            {...getInputProps()}
+          />
+          <button
+            aria-label="clear selection"
+            className="px-2"
+            type="button"
+            onClick={() => {
+              setSuggestions([]);
+            }}
+            tabIndex={-1}
+          >
+            &#215;
+          </button>
+          <button
+            aria-label="toggle menu"
+            className="px-2"
+            type="button"
+            {...getToggleButtonProps()}
+          >
+            {isOpen ? <>&#8593;</> : <>&#8595;</>}
+          </button>
+        </div>
       </div>
-    </>
+      <ul
+        className={`absolute w-72 bg-white mt-1 shadow-md max-h-80 overflow-scroll p-0 ${
+          !(isOpen && suggestions.length) && "hidden"
+        }`}
+        {...getMenuProps()}
+      >
+        {isOpen &&
+          suggestions.length > 0 &&
+          suggestions.map((item, index) => {
+            return (
+              <li
+                key={`${item.value}${index}`}
+                {...getItemProps({ item, index })}
+              >
+                <span>{item.package.name}</span>
+                <span className="text-sm text-gray-700">
+                  {item.package.description}
+                </span>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
   );
 };
